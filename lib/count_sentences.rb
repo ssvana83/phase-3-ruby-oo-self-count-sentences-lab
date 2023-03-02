50d34045f66c917f59f0e0fc660e0e40f8bf5f44
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-02 12:11:17 -0500

    commit tree ddfbcb4220ac4f24822ba76e74d405b31153cd9a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    elsif self.end_with?("")
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    elsif self.end_with?("")
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    elsif self.end_with?("")
+      false
+    end
   end
 
   def count_sentences
-
+    split = self.split(/[.+?+!+]\s/).count
   end
 end
